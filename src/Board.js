cfa0200f77ee49c0f29b8a34c14527c39b4bd285
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-08-26 14:27:39 -0500

    adds state to board component

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import Row from './Row';
 
 const Board = () => {
+  const [board, setBoard] = useState([]);
+  const [player, setPlayer] = useState({ char: null });
+  const [computer, setComputer] = useState({ char: null });
+
   return (
     <div className="board">
       <Row />
