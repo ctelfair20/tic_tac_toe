20941b570e883f43d05816b01183e35076161be6
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-08-29 13:59:04 -0500

    adds comments for the compTakesTurn function

--- src/box.js
+++ src/box.js
@@ -4,10 +4,19 @@ const Box = ({ player, computer, setPlayer }) => {
   const [filled, setFilled] = useState('');
   const [isFilled, setIsFilled] = useState(false);
 
+  const compTakesTrun = () => {
+    console.log('is the comps turn');
+    // have comp choose a random box
+    // fill that box with the comps character
+    // switch isturn to true
+    console.log('comp turn complete!');
+  }
+
   const handleClick = (e) => {
     setFilled(player.char);
     setIsFilled(true);
     setPlayer({ ...player, isTurn: !player.isTurn })
+    compTakesTrun();
   }
 
   return (
