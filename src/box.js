bce5d5b5aa19c26cc703f5c8464e174ee39ede1d
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-09-01 23:41:42 -0500

    sets box to use the board in state and not the filled variable in state

--- src/box.js
+++ src/box.js
@@ -1,53 +1,51 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Box = ({ id, player, computer, setPlayer, board, setBoard }) => {
   const [filled, setFilled] = useState('');
   const [isFilled, setIsFilled] = useState(false);
 
-  const updateBoard = (id) => {
-    setBoard({ ...board, [id]: player.char });
-  }
+  // const updateBoard = (id) => {
+  //   setBoard({ ...board, [id]: player.char });
+  // }
 
   const compTakesTrun = () => {
-    console.log('is the comps turn');
     // find all the empty boxes
     // create an array to hold the empty boxes
     let emptyBoxesIds = [];
+    console.log('board', board);
     // iterate over the board state
-    Object.keys(board).map((id) => {
-      console.log(id);
-      // if (board[id] === '') {
-      //   emptyBoxesIds.push(id);
-      // }
+    Object.keys(board).map((boxId) => {
+      // if box is empty
+      if (board[boxId] === '') {
+        // push box to array
+        emptyBoxesIds.push(boxId);
+      }
     });
-    // console.log(emptyBoxesIds);
-    // if box is empty
-    // push box to array
-    // have comp choose a random box
-    // use the length of the array and math.random to find one empty box
-    // fill that box with the comps character
-    // update fill var with comps character
-    // update board with this box
-    // update player turn to be true
-    console.log('comp turn complete!');
+    console.log('options', emptyBoxesIds);
+    // // have comp choose a random box
+    // // use the length of the array and math.random to find one empty box
+    // const randomBox = Math.floor(Math.random() * emptyBoxesIds.length).toString();
+    // console.log('num', randomBox);
+    // // fill that box with the comps character
+    // setBoard({ ...board, [randomBox]: computer.char })
+    // // update fill var with comps character
+    // // update board with this box
+    // // update player turn to be true
+    // setPlayer({ ...player, isTurn: true })
+    // console.log('comp turn complete!');
   }
 
   const handleClick = (e) => {
-    // fills in box chosen by user
-    setFilled(player.char);
-    // tells the computer this box is taken
-    setIsFilled(true);
     // ensure the state of the board is up-to-date
-    updateBoard(id);
+    setBoard({ ...board, [id]: player.char });
     // lets the computer know when to take its turn
     setPlayer({ ...player, isTurn: !player.isTurn });
-    // console.log('handle click board :', board);
-    compTakesTrun();
+    // compTakesTrun();
   }
 
   return (
     <div className="box" onClick={handleClick}>
-      <span className="filled">{filled}</span>
+      <span className="filled">{board[id]}</span>
     </div>
   );
 }
