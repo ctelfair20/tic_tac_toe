a665d78414e6c5e2ffc6b5071ef23bab16c8a04b
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-09-09 00:26:54 -0500

    moves useEffect up on board component to prevent multiple renders

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import Row from './Row';
 import Box from './Box';
 
 const Board = ({ player, computer, setPlayer }) => {
   const [board, setBoard] = useState({ 0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '' });
+  const [options, setOptions] = useState([]);
+
+
+  useEffect(() => {
+    console.log('board', board);
+    if (!player.isTurn) {
+      // create an array to hold the empty boxes
+      let emptyBoxesIds = [];
+      // iterate over the board state
+      Object.keys(board).forEach((boxId) => {
+        // if box is empty
+        if (board[boxId] === '') {
+          // push box to array
+          emptyBoxesIds.push(boxId);
+        }
+      });
+      setOptions(emptyBoxesIds);
+      const randomBoxId = Math.floor(Math.random() * emptyBoxesIds.length).toString();
+      const randomBox = emptyBoxesIds[randomBoxId];
+      setBoard({ ...board, [randomBox]: computer.char })
+      setPlayer({ ...player, isTurn: true })
+    }
+  }, [board]);
 
   const mapBoxes = (array) => {
     return array.map((num) => {
--- src/box.js
+++ src/box.js
@@ -1,46 +1,25 @@
 import { useState, useEffect } from 'react';
 
 const Box = ({ id, player, computer, setPlayer, board, setBoard }) => {
-  const [options, setOptions] = useState([]);
+  // const [options, setOptions] = useState([]);
   // const [filled, setFilled] = useState('');
   // const [isFilled, setIsFilled] = useState(false);
 
-  useEffect(() => {
-    if (!player.isTurn) {
-      console.log("effects!!");
-      // create an array to hold the empty boxes
-      let emptyBoxesIds = [];
-      // iterate over the board state
-      Object.keys(board).forEach((boxId) => {
-        // if box is empty
-        if (board[boxId] === '') {
-          // push box to array
-          emptyBoxesIds.push(boxId);
-        }
-      });
-      setOptions(emptyBoxesIds);
-      const randomBoxId = Math.floor(Math.random() * emptyBoxesIds.length).toString();
-      const randomBox = emptyBoxesIds[randomBoxId];
-      setBoard({ ...board, [randomBox]: computer.char })
-      setPlayer({ ...player, isTurn: true })
-    }
-  }, [board]);
+  // const compTakesTrun = () => {
+  //   console.log('comp turn start!', options);
+  //   // have comp choose a random box
+  //   // use the length of the array and math.random to find one empty box
+  //   const randomBox = Math.floor(Math.random() * options.length).toString();
+  //   console.log('num', randomBox);
+  // fill that box with the comps character
+  // setBoard({ ...board, [randomBox]: computer.char })
+  // console.log('boardC', board);
+  // // update fill var with comps character
+  // // update board with this box
+  // // update player turn to be true
+  // setPlayer({ ...player, isTurn: true })
+  // console.log('comp turn complete!');
 
-  const compTakesTrun = () => {
-    console.log('comp turn start!', options);
-    // have comp choose a random box
-    // use the length of the array and math.random to find one empty box
-    const randomBox = Math.floor(Math.random() * options.length).toString();
-    console.log('num', randomBox);
-    // fill that box with the comps character
-    // setBoard({ ...board, [randomBox]: computer.char })
-    // console.log('boardC', board);
-    // // update fill var with comps character
-    // // update board with this box
-    // // update player turn to be true
-    // setPlayer({ ...player, isTurn: true })
-    // console.log('comp turn complete!');
-  }
 
   const handleClick = (e) => {
     // ensure the state of the board is up-to-date
@@ -48,7 +27,7 @@ const Box = ({ id, player, computer, setPlayer, board, setBoard }) => {
     // lets the computer know when to take its turn
     setPlayer({ ...player, isTurn: !player.isTurn });
     ///////////////////////think about using useEffect to tell the computer to take its turn//////////
-    compTakesTrun();
+    // compTakesTrun();
   }
 
   return (
