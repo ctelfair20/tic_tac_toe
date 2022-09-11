d54533dc8e751100e8dd589b78be81b960859b50
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-09-11 17:27:27 -0500

    change turn only if the game is still going

--- src/box.js
+++ src/box.js
@@ -1,12 +1,15 @@
-const Box = ({ id, player, computer, setPlayer, board, setBoard }) => {
+const Box = ({ id, player, computer, setPlayer, board, setBoard, setWinnerOptionsWrapper, setWinnerOptions, isOver }) => {
 
   const handleClick = (e) => {
     // check if it is the player's trun
     if (player.isTurn && player.char != null) {
       // ensure the state of the board is up-to-date
       setBoard({ ...board, [id]: player.char });
+      setWinnerOptionsWrapper(id, player.char);
       // lets the computer know when to take its turn
-      setPlayer({ ...player, isTurn: !player.isTurn });
+      if (!isOver) {
+        setPlayer({ ...player, isTurn: !player.isTurn });
+      }
     } else {
       console.log("not your turn");
     }
